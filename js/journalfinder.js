599e975ced76c4edc84efcdccd1de154d676a103
Matthew Reidsma <reidsmam@gvsu.edu>
2019-03-28 16:24:53 -0400

    Styled autosuggest menu which looked like garbage.

--- js/journalfinder.js
+++ js/journalfinder.js
@@ -2,3 +2,5 @@
 var alertDiv = document.querySelector('.SS_ToolNotes');
 alertDiv.className = "alert alert-info";
 alertDiv.style.marginTop = '1em';
+
+document.getElementById('submit-title-search-type').className = 'btn btn-primary ng-binding';
