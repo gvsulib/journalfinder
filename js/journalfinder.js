522ed4bb7aa22ebb20a3487ff2007edb0409341d
Matthew Reidsma <reidsmam@gvsu.edu>
2020-03-20 13:01:24 -0400

    Added alert code

--- js/journalfinder.js
+++ js/journalfinder.js
@@ -4,3 +4,8 @@ alertDiv.className = "alert alert-info";
 alertDiv.style.marginTop = '1em';
 
 document.getElementById('submit-title-search-type').className = 'btn btn-primary ng-binding';
+
+	// Alert adding
+	var alertScript = document.createElement('script');
+alertScript.src = 'https://prod.library.gvsu.edu/labs/alert/alert.js';
+document.body.appendChild(alertScript);
